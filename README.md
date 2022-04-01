f459c6b39a8dcc8d740e2d07f7aa2496c0705921
ligiacr <ligia_c_rodrigues@hotmail.com>
2022-04-01 00:21:11 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,4 @@
+## UAM - Estruturas de Dados
+
+:books: Realização das atividades A1 e A3 da disciplina Estruturas de Dados, curso ADS, da Universidade Anhembi Morumbi.
+
---
+++ a1/atividade1.java
@@ -0,0 +1,48 @@
+package a1;
+
+import java.util.Scanner;
+
+public class atividade1 {
+
+	public static void main(String[] args) {
+
+//		Criar uma estrutura heterogênea (array) do tipo string para cadastrar o nome
+//		de 25 alunos; Garantir que a estrutura seja totalmente preenchida.
+
+		String[] nomes = new String[25];
+		Scanner teclado = new Scanner(System.in);
+		int aux = 0;
+
+//		Recebe os nomes do usuário e insere sequencialmente no array
+		for (; aux < nomes.length; aux++) {
+			System.out.println("Insira o nome do aluno número " + (aux + 1) + ":");
+			nomes[aux] = teclado.nextLine();
+		}
+//		Exibe os nomes inseridos anteriormente
+		System.out.print("Alunos na lista: ");
+		for (String aluno : nomes) {
+			System.out.print(aluno + " | ");
+		}
+
+//		 Após implementar o programa e preencher o array com o nome dos alunos,
+//		 cadastre mais 5 novos alunos. (Obs.: exceção esperada ao rodar este trecho de
+//		 código, pois o array já atingiu a capacidade máxima definida.)
+
+		System.out.println();
+		System.out.println();
+
+//		Tenta inserir o próximo nome digitado, que excede a capacidade do array e lança a exceção.
+		for (; aux < 29; aux++) {
+			System.out.println("Insira o nome do aluno número " + (aux + 1) + ":");
+
+			nomes[aux] = teclado.nextLine();
+			teclado.close();
+		}
+		System.out.print("Alunos na lista: ");
+		for (String aluno : nomes) {
+			System.out.print(aluno + " | ");
+		}
+
+	}
+
+}
